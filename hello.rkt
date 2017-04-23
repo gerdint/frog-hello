a3d59c870c2da353db3efde93408dd0aa9149a10
Tobias Gerdin <tgerdin@gmail.com>
2017-04-23 16:10:15 +0200

    Make use of config variable

--- hello.rkt
+++ hello.rkt
@@ -34,14 +34,17 @@
   ;; Tests to be run with raco test
   )
 
+(define greeting (get-config 'greeting "Hola"))
+
 (enhance-body
  (λ (xs)
    (displayln "Enhancing body")
    xs))
 
-(add-cleanup
+(register-cleanup
  (λ ()
    (displayln "Cleaning up")))
 
 ;;; Code in module body is evaluated when module is loaded at Frog startup
-(displayln "Hello Frog!")
+(displayln (string-append greeting " Frog!"))
+
